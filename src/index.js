009d71f04ff76740cf32485a193fec892427c5bb
JWCosta <jonathanwillian80@hotmail.com>
2020-04-25 01:25:19 -0300

    Finalizado

---
+++ src/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const fibonacci = () => {
+let fArr = []; //Criação do array
+let element = 0;
+
+for (let i = 0;; i++) { // o "i" é o incremento utilizado para formar o valor de element
+    if(i < 2){ //Se o incremento for menor que 2 ele é inserido no array
+        fArr.push(i); //adicionando o incremento no array
+        continue
+    }
+    element = fArr[i-1] + fArr[i-2] //composição do element juntamente com o incremento
+
+    if( element > 400){  //Se a o element for maior que 400 o if dispara o break que interrompe o laço de repetição
+        break;
+    }
+    fArr.push(element) //adicionando o element no array
+}
+    return fArr; 
+}
+
+console.log(fibonacci());
+
+
+
+const isFibonnaci = (num) => {
+    const array = fibonacci();
+    const res = array.includes(num); // O includes verificar se certo elemento está contido ou não no array
+
+    return res;
+}
+
+console.log(isFibonnaci(8)); //Inserir entre parênteses o número para ser ver se está contido na sequência
+
+
+module.exports = { //exportação dos metódos para serem utilizados em outros arquivos
+    fibonacci,
+    isFibonnaci
+}
